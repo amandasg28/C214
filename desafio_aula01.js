f7ea79e41012825aab4b3231a5016620713d6b85
Amanda Silva Guimarães <amanda.silva@gec.inatel.br>
2023-08-16 15:11:26 -0300

    update commit

---
+++ desafio_aula01.js
@@ -0,0 +1,43 @@
+class Filme {
+    constructor(titulo, ano, duracao, genero) {
+        this.titulo = titulo;
+        this.ano = ano;
+        this.genero = genero;
+        this.duracao = duracao;
+        this.assistir = false;
+        this.avaliar = null;
+    }
+  
+    detalhes() {
+        console.log(`Filme:
+            Título: ${this.titulo}
+            Ano: ${this.ano}
+            Duração: ${this.duracao} 
+            Gênero: ${this.genero}
+            Assistido: ${this.assistido ? 'Sim' : 'Não'}
+            Avaliação: ${this.avaliacao}`);
+    }
+  
+    assistido() {
+        this.assistir = true;
+    }
+  
+    avaliado(nota) {
+        this.avaliar = nota;
+    }
+  }
+  
+  const filme1 = new Filme ('Homem Aranha', 2007, '2h20m', 'Ação/Aventura')
+  filme1.assistido();
+  filme1.avaliado(9);
+  filme1.detalhes();
+  
+  const filme2 = new Filme ('Mulher Maravilha', 2017, '2h21m', 'Ação/Aventura')
+  filme2.assistido();
+  filme2.avaliado(8.5);
+  filme2.detalhes();
+  
+  const filme3 = new Filme ('Capitão América', 2011, '2h04m', 'Ação/Aventura')
+  filme3.assistido();
+  filme3.avaliado(5.0);
+  filme3.detalhes();
