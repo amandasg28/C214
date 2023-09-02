e93995ae10b6138bd64dbf149bbd8bf3251ab4f0
Amanda Silva Guimarães <amanda.silva@gec.inatel.br>
2023-09-01 21:40:30 -0300

    commit

---
+++ app_imc/README.md
@@ -0,0 +1,16 @@
+
+## Cáculo de IMC
+
+Na aplicação o usuário insere sua altur e peso para que seja calculado o IMC.
+Assim que o IMC é calculado o background muda de cor, aparece na tela o próprio IMC e também qual o índice que a pessoa se encontra conforme a tabela abaixo:
+
+### Tabela IMC
+
+| IMC |Classificação|Cor|
+| ----------------- | ------------------------------------------------------------------ | -----------|
+|  Abaixo de 18,5 | Abaixo do Peso | ![#FAB733](https://via.placeholder.com/10/FAB733?text=+) #FAB733 |
+| Entre 18,6 e 24,9 |Peso Ideal | ![#69B34C](https://via.placeholder.com/10/69B34C?text=+) #69B34C |
+| Entre 25 e 29,9 | Levemente acima do peso | ![#ACB334](https://via.placeholder.com/10/ACB334?text=+) #ACB334|
+| Entre 30 e 34,9| Obesidade grau 1 | ![#FF8E15](https://via.placeholder.com/10/FF8E15?text=+) #FF8E15|
+| Entre 35 e 39,9| Obesidade grau 2 |![#FF4E11](https://via.placeholder.com/10/FF4E11?text=+) #FF4E11| 
+| Acima de 40 | Obesidade grau 3 | ![#FF0D0D](https://via.placeholder.com/10/FF0D0D?text=+) #FF0D0D 
---
+++ app_imc/src/App.svelte
@@ -0,0 +1,76 @@
+<script>
+	let peso = 0;
+	let altura = 0;
+	let imc = 0;
+
+	function calcularIMC() {
+		if (peso > 0 && altura > 0) {
+			imc = (peso / Math.pow(altura, 2)).toFixed(1);
+		} else imc = 0;
+	}
+</script>
+
+<main>
+	<h1>Calculadora de IMC</h1>
+	<label>
+		Peso (kg): <input
+			type="number"
+			bind:value={peso}
+			on:input={calcularIMC}
+		/>
+	</label>
+	<label>
+		Altura (m): <input
+			type="number"
+			bind:value={altura}
+			on:input={calcularIMC}
+		/>
+	</label>
+	{#if imc > 0}
+		<p>
+			Seu IMC é {imc} 
+			{#if imc <= 18.5}
+				<b Abaixo do peso</b>
+			{:else if imc <= 24.9}
+				<b class="verde">Peso ideal</b>
+			{:else if imc <= 29.9}
+				<b Levemente acima do peso</b>
+			{:else if imc <= 34.9}
+				<b Obesidade grau I</b>
+			{:else if imc <= 39.9}
+				<b Obesidade grau II</b>
+			{:else}
+				<b Obesidade grau III</b>
+			{/if}
+		</p>
+	{/if}
+</main>
+
+<style>
+	main {
+		font-family: sans-serif;
+		text-align: center;
+		padding: 2em;
+	}
+
+	h1 {
+		font-size: 1.5em;
+		margin-bottom: 1rem;
+	}
+
+	label {
+		display: block;
+		margin-bottom: 0.5rem;
+	}
+
+	input {
+		padding: 0.25rem;
+	}
+
+	.verde {
+		color: green;
+    background-color: rgba(0, 0, 0, 0.1);
+		padding: 0.1em 0.3em;
+		text-shadow: 1px 1px 0px black;
+	}
+</style>
